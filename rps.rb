bf445fc19a03b5ad676ac0c951077f83f853d78b
Eric Hagemann <hagemann.eric@gmail.com>
2015-10-26 08:08:43 -0400

    Added winning message method

--- rps.rb
+++ rps.rb
@@ -2,6 +2,17 @@
 
 puts "Play Rock, Paper, Scissors!"
 
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'P'
+    puts "Paper covers rock!"
+  when 'S'
+    puts "Scissors cuts paper!"
+  when 'R'
+    puts "Rock smashes scissors!"
+  end
+end
+
 loop do
   begin
     puts "Choose one: (R/P/S)"
@@ -10,12 +21,15 @@ loop do
   computer_choice = %w(R P S).sample
   puts "You picked #{player_choice} and computer picked #{computer_choice}."
   result = player_choice + computer_choice
+
   case result
   when 'RR' || 'SS' || 'PP'
     puts "It's a tie."
   when 'RS' || 'PR' || 'SP'
+    display_winning_message(player_choice)
     puts "You won!"
   else
+    display_winning_message(computer_choice)
     puts "You lost!"
   end
   puts "Play again? (Y/N)"
